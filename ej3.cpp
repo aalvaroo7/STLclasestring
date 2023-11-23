14a231de5f271b618f72ef4ba26057b121064b1d
aalvaroo7 <almarrom@myuax.com>
2023-11-23 12:27:39 +0100

    cruz cafune>aitana

---
+++ ej3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+    string frase = "hola";
+    string frase2 = " y adios";
+    string frase3 = frase + frase2;
+    cout << "la frase resultante es:" << frase3 << endl;
+}
