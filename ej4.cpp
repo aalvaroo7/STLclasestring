527515494a201eb39240885066977233c63b8f23
aalvaroo7 <almarrom@myuax.com>
2023-11-23 12:41:58 +0100

    cruz cafune>aitana

--- ej4.cpp
+++ ej4.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <string>
-using namespace std;
-int main(){
-    string frase = "hola";
-    string sub_str = "ho";
-    string::size_type pos = frase.find(sub_str);
-    if(pos != string::npos){
-        cout << "la subcadena " << sub_str << " está en la posición " << pos << endl;
-    }
-    else{
-        cout << "la subcadena " << sub_str << " no está en la cadena" << endl;
 
+int main() {
+    // Definimos la cadena inicial
+    std::string frase = "hola";
+
+    // Definimos la subcadena que queremos buscar y reemplazar
+    std::string sub_str = "ho";
+
+    // Buscamos la posición de la subcadena en la cadena principal
+    std::string::size_type pos = frase.find(sub_str);
+
+    if (pos != std::string::npos) {
+        // Imprimimos la posición si la subcadena se encuentra en la cadena principal
+        std::cout << "La subcadena '" << sub_str << "' está en la posición " << pos << std::endl;
+
+        // Reemplazamos la subcadena encontrada por otra subcadena
         std::string nueva_subcadena = "hi";
         frase.replace(pos, sub_str.length(), nueva_subcadena);
 
+        // Mostramos la cadena resultante después del reemplazo
         std::cout << "Cadena resultante: " << frase << std::endl;
-    }
-    else {
+    } else {
+        // Imprimimos un mensaje si la subcadena no se encuentra en la cadena principal
         std::cout << "La subcadena '" << sub_str << "' no está en la cadena" << std::endl;
     }
 
