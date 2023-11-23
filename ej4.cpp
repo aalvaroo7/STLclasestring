66dca1c3c3a47ad0265d4c836ff462f84d690109
aalvaroo7 <almarrom@myuax.com>
2023-11-23 12:33:16 +0100

    cruz cafune>aitana

--- ej4.cpp
+++ ej4.cpp
@@ -2,3 +2,14 @@
 #include <vector>
 #include <string>
 using namespace std;
+int main(){
+    string frase = "hola";
+    string sub_str = "ho";
+    string::size_type pos = frase.find(sub_str);
+    if(pos != string::npos){
+        cout << "la subcadena " << sub_str << " está en la posición " << pos << endl;
+    }
+    else{
+        cout << "la subcadena " << sub_str << " no está en la cadena" << endl;
+    }
+}
