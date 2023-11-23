ab8b39eb5062a435addce4e10fc45362927882ad
aalvaroo7 <almarrom@myuax.com>
2023-11-23 12:20:08 +0100

    cruz cafune>aitana

--- ej1y2.cpp
+++ ej1y2.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <string>
 using namespace std;
-int main() {
-   string phrase = "Hello, World!";
-   cout << phrase << std::endl;
-    return 0;
- size(phrase);
-empty(phrase);
+    int main() {
+       string sentence = "OpenAI is a leading AI research lab.";
+       cout << "Cadena: " << sentence << std::endl;
+        cout << "Tamaño de la cadena: " << sentence.size() << std::endl;
+        cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << std::endl;
+        return 0;
+
+    }
