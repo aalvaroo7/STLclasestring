4cee93a754183f27c19379f1b6f31fc1530dca6e
aalvaroo7 <almarrom@myuax.com>
2023-11-23 12:15:14 +0100

    cruz cafune>aitana

--- ej1y2.cpp
+++ ej1y2.cpp
@@ -1,9 +1,11 @@
+
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 int main() {
    string phrase = "Hello, World!";
    cout << phrase << std::endl;
     return 0;
-
-}
+ size(phrase);
+empty(phrase);
